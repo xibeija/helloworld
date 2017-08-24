c8d86c6ae059272d5c0b77ba2dd5d6d46725d8da
xibeija <xiaoxibei163@163.com>
2017-08-24 19:19:35 +0800

    Initial commit

---
+++ src/com/fz/User.java
@@ -0,0 +1,10 @@
+package com.fz;
+
+/**
+ * Created by webrx on 2017-08-24.
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello world .............");
+    }
+}
