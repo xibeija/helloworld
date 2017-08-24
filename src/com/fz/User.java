fff66b931805e9d30f7e9c73a73ae41170b5a588
xibeija <xiaoxibei163@163.com>
2017-08-24 20:16:30 +0800

    hello world

--- src/com/fz/User.java
+++ src/com/fz/User.java
@@ -6,5 +6,6 @@ package com.fz;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello world .............");
+        System.out.println("************");
     }
 }
